4f71f0c3e3809c059698146506dd9499b75e0a4d
eipc16 <pprzemek.312@gmail.com>
2019-06-05 10:17:46 +0200

    init

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -1,7 +1,7 @@
 import json, os, imageio
 import numpy as np
 
-from models.points import Point
+from models.points import Point, KeyPoint
 from json_utils import JSONHelper
 
 
@@ -12,6 +12,25 @@ class FileHelper:
 
         if not os.path.exists(self._path):
             os.makedirs(self._path)
+
+    def load_key_points(self, path_name):
+
+        def _parse_keypoint(line):
+            items = line.split(" ")
+            x, y = float(items[0]), float(items[1])
+            
+            features = map(items[5:], lambda x: int(x))
+
+            return KeyPoint(x, y, features)
+
+
+        path = f"{self._path}/{path_name}"
+       
+        with open(path, mode='r') as f:
+            content = f.readlines()[2:]
+            keyPoints = map(content, lambda x : _parse_keypoint(x))
+
+
 
     def save_as_json(self, dest_path, obj_list):
         path = f"{self._path}/{dest_path}"
