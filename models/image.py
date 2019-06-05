99d51c53017bf7a79cdd3717d907a472015a2902
eipc16 <pprzemek.312@gmail.com>
2019-06-05 12:24:00 +0200

    added image distance

--- models/image.py
+++ models/image.py
@@ -1,3 +1,15 @@
+import numpy as np
+
 class Image:
     def __init__(self, keypoints):
         self._keypoints = keypoints
+
+    def get_keypoints_pairs(self, picture):
+
+    def nearest_keypoint_index(keypoint, keypoint_list):
+        distances = list(map(lambda x: distance(keypoint, x), keypoint_list))
+        return keypoint_list.index(min(distances))
+
+    def distance(point1, point2):
+        feature_pairs = zip(point1.feautres, point2.features)
+        return np.sqrt(reduce(lambda x: np.square(x[0] - x[1]), feature_pairs))
